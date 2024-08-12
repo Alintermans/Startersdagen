413e65694a19b45c553022b268268fe8620b5d9a
Anton Lintermans <anton.lintermans@student.kuleuven.be>
2024-08-12 12:15:04 +0200

    Update script

--- setup_informatie.cmd
+++
@@ -1,6 +0,0 @@
-REM Activate environment and run script
-call conda activate dlib
-REM cd C:\Users\Public\Documents\Startersdagen\Informatie
-cd C:\Users\k00140012\Desktop\Startersdagen\Informatie
-REM python C:\Users\Public\Documents\Startersdagen\Informatie\Server.py
-python Server.py
